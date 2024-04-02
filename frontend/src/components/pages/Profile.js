cc7c10ed0a172d150ec124c79fc6d6db44bb83cb
manish mandal <76776194mm@gmail.com>
2024-04-02 20:16:13 +0530

    complete user photo upload functionality

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { updateUserSuccess, setActiveAccount, setLogoutAccount } from '../../redux/user/userSlice';
 import DeactivateAccount from '../common/DeactivateAccount';
 import LogoutAccount from '../common/LogoutAccount';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from '../../firebase';
 
 const Profile = () => {
     const { currentUser, deactivateAccount, logoutAccount } = useSelector(state => state.user);
 
     const dispatch = useDispatch();
-
-    const [formData, setFormData] = useState();
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
     const [updateLoading, setUpdateLoading] = useState(false);
 
     const handleChange = (e) => {
@@ -20,9 +25,33 @@ const Profile = () => {
         });
     };
 
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                // console.log('Upload is ' + progress + '% done');
+                setFilePerc(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+                    setFormData({ ...formData, photo: downloadURL })
+                );
+            }
+        );
+
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        console.log(formData)
         try {
             setUpdateLoading(true);
             const res = await fetch(`/api/users/updateMe/${currentUser._id}`, {
@@ -48,16 +77,34 @@ const Profile = () => {
         }
     }
 
-
-
-
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
 
-                <img src={currentUser.photo} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                {/* <img src={currentUser.photo} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' /> */}
+
+                <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*' />
+                <img onClick={() => fileRef.current.click()} src={formData.photo || currentUser.photo} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+                <p className='text-sm self-center'>
+                    {fileUploadError ? (
+                        <span className='text-red-700'>
+                            Error Image upload (image must be less than 2 mb)
+                        </span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+                    ) : filePerc === 100 ? (
+                        <span className='text-green-700'>Image successfully uploaded!</span>
+                    ) : (
+                        ''
+                    )}
+                </p>
 
                 <input type='text' placeholder='username' id='name' defaultValue={currentUser.name} className='border p-3 rounded-lg' onChange={handleChange} />
                 <input type='text' placeholder='email' id='email' defaultValue={currentUser.email} className='border p-3 rounded-lg' onChange={handleChange} />
